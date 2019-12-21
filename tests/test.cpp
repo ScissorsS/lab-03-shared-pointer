20150d645d815253e730fb1de3b25d3ea90d582e
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2019-12-21 10:33:48 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,45 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "header.hpp"
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+char *test_ptr1 = new char;
+*test_ptr1 = 'a';
+
+SharedPtr<char> pointer1(test_ptr1);
+EXPECT_EQ(pointer1.get(), test_ptr1);
+EXPECT_EQ(pointer1.use_count(), 1);
+
+SharedPtr<char> pointer2(pointer1);
+EXPECT_EQ(pointer2.get(), pointer1.get());
+EXPECT_EQ(pointer1.use_count(), 2);
+EXPECT_EQ(pointer2.use_count(), 2);
+
+char *test_ptr2 = new char;
+*test_ptr2 = 'b';
+SharedPtr<char> pointer3(test_ptr2);
+EXPECT_EQ(*pointer3, *test_ptr2);
+
+pointer2 = pointer3;
+EXPECT_EQ(pointer2.get(), pointer3.get());
+
+pointer1.Swap(pointer3);
+EXPECT_EQ(pointer1.get(), test_ptr2);
+EXPECT_EQ(pointer3.get(), test_ptr1);
+
+pointer3.reset();
+EXPECT_EQ(pointer3.use_count(), 0);
+EXPECT_EQ(pointer3.get(), nullptr);
+
+//pointer2.reset(test_ptr1);
+//EXPECT_EQ(pointer2.get(), test_ptr1);
+EXPECT_EQ(pointer1.use_count(), 2);
+EXPECT_EQ(pointer2.use_count(), 2);
+pointer1.reset();
+pointer2.reset();
+}
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
