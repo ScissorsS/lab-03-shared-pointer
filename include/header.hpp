6ccb12dbd00801c19febc94a9e3bbd85b5b90781
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2019-12-22 15:56:17 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -14,41 +14,41 @@ using std::atomic_uint;
 template <typename T>
 class SharedPtr {
 public:
-    SharedPtr()
-    {
+    
+    SharedPtr() {
         _ptr = nullptr;
     }
-    explicit SharedPtr(T* ptr)
-    {
+    
+    explicit SharedPtr(T* ptr) {
         _ptr = ptr;
         SharedPtr::data_base[_ptr] = new atomic_uint(1);
     }
-    explicit SharedPtr(const SharedPtr& r)
-    {
+    
+    explicit SharedPtr(const SharedPtr& r) {
         _ptr = r._ptr;
         if (_ptr != nullptr)
             (*SharedPtr::data_base[_ptr])++;
     }
-    explicit SharedPtr(SharedPtr&& r)
-    {
+    
+    explicit SharedPtr(SharedPtr&& r) {
         _ptr = r._ptr;
         r._ptr = nullptr;
     }
-    ~SharedPtr()
-    {
+    
+    ~SharedPtr() {
         if (_ptr == nullptr)
             return;
         if (SharedPtr::data_base[_ptr] == nullptr)
             _ptr = nullptr;
         (*SharedPtr::data_base[_ptr])--;
-        if ((*SharedPtr::data_base[_ptr]) == 0)
-        {
+        if ((*SharedPtr::data_base[_ptr]) == 0) {
             delete SharedPtr::data_base[_ptr];
             SharedPtr::data_base.erase(_ptr);
             delete _ptr;
         }
         _ptr = nullptr;
     }
+    
     auto operator=(const SharedPtr& r) -> SharedPtr& {
         if (&r == this)
             return *this;
@@ -58,6 +58,7 @@ public:
             (*SharedPtr::data_base[_ptr])++;
         return *this;
     }
+    
     auto operator=(SharedPtr&& r) -> SharedPtr& {
         if (&r == this)
             return *this;
@@ -68,29 +69,27 @@ public:
         return *this;
     }
 
-    operator bool() const
-    {
+    operator bool() const {
         return (_ptr != nullptr);
     }
-    auto operator*() const -> T&
-    {
+    
+    auto operator*() const -> T& {
         return *_ptr;
     }
-    auto operator->() const -> T*
-    {
+    
+    auto operator->() const -> T* {
         return _ptr;
     }
 
-    auto get() -> T*
-    {
+    auto get() -> T* {
         return _ptr;
     }
-    void reset()
-    {
+    
+    void reset() {
          this->~SharedPtr();
     }
-    void reset(T* ptr)
-    {
+    
+    void reset(T* ptr) {
         if (_ptr != nullptr)
             if ((*SharedPtr::data_base[_ptr]) != 0)
                 (*SharedPtr::data_base[_ptr])--;
@@ -102,8 +101,8 @@ public:
             (*SharedPtr::data_base[_ptr])++;
         SharedPtr::data_base[_ptr] = new atomic_uint(1);
     }
-    void Swap(SharedPtr& r)
-    {
+    
+    void Swap(SharedPtr& r) {
         T* temp;
         temp = this->_ptr;
         this->_ptr = r._ptr;
@@ -111,16 +110,16 @@ public:
         temp = nullptr;
     }
      
-    auto use_count() const -> size_t
-    {
+    auto use_count() const -> size_t {
         if (_ptr == nullptr)
             return 0;
         size_t number = (*SharedPtr::data_base[_ptr]);
         return number;
     }
+    
     static std::map <T*, std::atomic_uint*> data_base;
-
 private:
+    
     T* _ptr;
 };
 
