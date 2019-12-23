4c493f20f074606f00ed2e574623c43e496d8720
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2019-12-24 00:00:24 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -14,33 +14,35 @@ using std::atomic_uint;
 template <typename T>
 class SharedPtr {
 public:
-    SharedPtr() {
+    SharedPtr()
+    {
         _ptr = nullptr;
     }
-    explicit SharedPtr(T* ptr) {
+    explicit SharedPtr(T* ptr)
+    {
         _ptr = ptr;
         SharedPtr::data_base[_ptr] = new atomic_uint(1);
     }
-    explicit SharedPtr(const SharedPtr& r) {
+    explicit SharedPtr(const SharedPtr& r)
+    {
         _ptr = r._ptr;
         if (_ptr != nullptr)
             (*SharedPtr::data_base[_ptr])++;
     }
-    explicit SharedPtr(SharedPtr&& r) {
+    explicit SharedPtr(SharedPtr&& r)
+    {
         _ptr = r._ptr;
         r._ptr = nullptr;
     }
-    ~SharedPtr() {
-        if (SharedPtr::data_base.find(_ptr)
-            == SharedPtr::data_base.end()) {
-            return;
-        }
+    ~SharedPtr()
+    {
         if (_ptr == nullptr)
             return;
         if (SharedPtr::data_base[_ptr] == nullptr)
             _ptr = nullptr;
         (*SharedPtr::data_base[_ptr])--;
-        if ((*SharedPtr::data_base[_ptr]) == 0) {
+        if ((*SharedPtr::data_base[_ptr]) == 0)
+        {
             delete SharedPtr::data_base[_ptr];
             SharedPtr::data_base.erase(_ptr);
             delete _ptr;
@@ -48,10 +50,6 @@ public:
         _ptr = nullptr;
     }
     auto operator=(const SharedPtr& r) -> SharedPtr& {
-        if (SharedPtr::data_base.find(r._ptr)
-            == SharedPtr::data_base.end()) {
-            return *this;
-        }
         if (&r == this)
             return *this;
         this->~SharedPtr();
@@ -61,10 +59,6 @@ public:
         return *this;
     }
     auto operator=(SharedPtr&& r) -> SharedPtr& {
-        if (SharedPtr::data_base.find(r._ptr)
-            == SharedPtr::data_base.end()) {
-            return *this;
-        }
         if (&r == this)
             return *this;
         this->~SharedPtr();
@@ -73,56 +67,49 @@ public:
         r._ptr = nullptr;
         return *this;
     }
-    operator bool() const {
+    operator bool() const
+    {
         return (_ptr != nullptr);
     }
-    auto operator*() const -> T& {
+    auto operator*() const -> T&
+    {
         return *_ptr;
     }
-    auto operator->() const -> T* {
+    auto operator->() const -> T*
+    {
         return _ptr;
     }
-    auto get() -> T* {
+    auto get() -> T*
+    {
         return _ptr;
     }
-    void reset() {
-        this->~SharedPtr();
+    void reset()
+    {
+         this->~SharedPtr();
+    }
+    void reset(T* ptr)
+    {
         if (_ptr != nullptr)
-            delete _ptr;
-    }
-    void reset(T* ptr) {
-        if (_ptr != nullptr) {
-            if ((*SharedPtr::data_base[_ptr]) != 0) {
-                delete SharedPtr::data_base[_ptr];
-                SharedPtr::data_base.erase(_ptr);
-                delete _ptr;
-                //_ptr = nullptr;
-            }
-        }
-        //_ptr = nullptr;
+            if ((*SharedPtr::data_base[_ptr]) != 0)
+                (*SharedPtr::data_base[_ptr])--;
+        _ptr = nullptr;
         if (ptr == nullptr)
             return;
-        //_ptr = ptr;
-        //if ((*SharedPtr::data_base[_ptr]))
-            //(*SharedPtr::data_base[_ptr])++;
-        //SharedPtr::data_base[_ptr] = new atomic_uint(1);
+        _ptr = ptr;
+        if ((*SharedPtr::data_base[_ptr]))
+            (*SharedPtr::data_base[_ptr])++;
+        SharedPtr::data_base[_ptr] = new atomic_uint(1);
     }
-    void Swap(SharedPtr& r) {
-        if (SharedPtr::data_base.find(r._ptr)
-            == SharedPtr::data_base.end()) {
-            return;
-        }
+    void Swap(SharedPtr& r)
+    {
         T* temp;
         temp = this->_ptr;
         this->_ptr = r._ptr;
         r._ptr = temp;
         temp = nullptr;
     }
-    auto use_count() const -> size_t {
-        if (SharedPtr::data_base.find(_ptr)
-            == SharedPtr::data_base.end()) {
-            return 0;
-        }
+    auto use_count() const -> size_t
+    {
         if (_ptr == nullptr)
             return 0;
         size_t number = (*SharedPtr::data_base[_ptr]);
