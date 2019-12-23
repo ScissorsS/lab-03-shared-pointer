f742d4e85358854c48a1dd43c55ab625e5318812
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2019-12-23 23:13:29 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,7 +5,6 @@
 
 TEST(Example, EmptyTest) {
 char *test_ptr1 = new char;
-char *test_ptr2 = new char;
 *test_ptr1 = 'a';
 
 SharedPtr<char> pointer1(test_ptr1);
@@ -17,6 +16,7 @@ EXPECT_EQ(pointer2.get(), pointer1.get());
 EXPECT_EQ(pointer1.use_count(), 2);
 EXPECT_EQ(pointer2.use_count(), 2);
 
+char *test_ptr2 = new char;
 *test_ptr2 = 'b';
 SharedPtr<char> pointer3(test_ptr2);
 EXPECT_EQ(*pointer3, *test_ptr2);
@@ -38,6 +38,7 @@ EXPECT_EQ(pointer1.use_count(), 0);
 EXPECT_EQ(pointer2.use_count(), 0);
 pointer1.reset();
 pointer2.reset();
+delete test_ptr2;
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
